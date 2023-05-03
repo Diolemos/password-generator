cf9dfaedc387d00354d754b7e638ca3ee2b37042
Dio Lemos <pedro.devsc@outlook.com>
2023-05-03 01:03:50 -0300

    create list of user numbers and symbols

--- gen.py
+++ gen.py
@@ -6,29 +6,33 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
 # create a list of user chosen letters
 user_letters = []
 for n in range(1,nr_letters + 1):
-    user_letters.append(letters[random.randint(0,len(letters)-1)])
+    user_letters.append(letters[random.randint(0,25)])
 
-
-print(user_letters)    
-    
-    
-
-    
-    
 
 #create a list of user chosen symbols
 
+user_symbols = []
+
+for n in range(1, nr_symbols + 1):
+    user_symbols.append(symbols[random.randint(0,len(symbols)-1)])
+
+
 #create a list of user chosen numbers
+user_numbers = []
+for n in range(1, nr_numbers + 1):
+    user_numbers.append(numbers[random.randint(0,10)])
 
-
+print(user_letters)
+print(user_numbers)
+print(user_symbols)
 #add all the items to the password_list, shuffle and stringify
 
 
