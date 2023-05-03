55a3acf553167704f4ccbaacf601637a532c394a
Dio Lemos <pedro.devsc@outlook.com>
2023-05-03 00:55:24 -0300

    create uuser_letters list variable

--- gen.py
+++ gen.py
@@ -6,12 +6,23 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
 # create a list of user chosen letters
+user_letters = []
+for n in range(1,nr_letters + 1):
+    user_letters.append(letters[random.randint(0,len(letters)-1)])
+
+
+print(user_letters)    
+    
+    
+
+    
+    
 
 #create a list of user chosen symbols
 
@@ -19,3 +30,7 @@ password_list = []
 
 
 #add all the items to the password_list, shuffle and stringify
+
+
+#create a password var for better readability 
+# password_list = password
