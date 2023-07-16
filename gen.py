a6393a85e551a71d3325b6ad0757221149b2e304
Dio Lemos <pedro.devsc@outlook.com>
2023-07-16 04:45:22 -0300

    update main.py

--- gen.py
+++ gen.py
@@ -1,6 +1,13 @@
+from password_generator import Password_generator
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+generator = Password_generator()
+
+password = generator.generate_password(num_of_letters=nr_letters,num_of_numbers=nr_numbers,number_of_symbols=nr_symbols)
+
+#print(password)
+print(f"Your password is:\n{password}")
--- password_generator.py
+++ password_generator.py
@@ -6,7 +6,7 @@ class Password_generator():
         self.NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
         
-        def generate_password(self,num_of_letters=5,num_of_numbers=3,number_of_symbols=3):
+    def generate_password(self,num_of_letters=5,num_of_numbers=3,number_of_symbols=3):
             letters = random.choices(self.LETTERS, k=num_of_letters)
             print(letters)
             # for _ in range(int(num_of_letters)):
